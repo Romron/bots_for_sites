9b3765774b3bb3946b3170927d0fc4ad62068006
romron@ukr.net <romron@ukr.net>
2021-03-05 15:31:34 +0200

    Черновой вариант в файле test.py

--- for_my_sites/for_prostofilm.ml/bot_PF_1/bot_PF_1.py
+++ for_my_sites/for_prostofilm.ml/bot_PF_1/bot_PF_1.py
@@ -22,19 +22,22 @@ def main_bot_PF_1(URL,browser):
 		
 		count_try_clicks_links = 0
 		while True:
+			rnd_link = random.choice(list)
 			try:
-				time.sleep(random.uniform(1,30))		# случайное число с плавающей точкой
-				random.choice(list).click()	# выбор случайной ссылки из списка и переход по ней
+				time.sleep(random.uniform(1,10))		# случайное число с плавающей точкой
+				
+				scroll_element(rnd_link,browser)
+
+				time.sleep(random.uniform(1,10))		# случайное число с плавающей точкой
+				rnd_link.click()	# выбор случайной ссbылки из списка и переход по ней
 				break
 			except Exception as e:
 				# raise e
 				count_try_clicks_links += 1
 				print(count_try_clicks_links, '. попытка кликнуть случайную ссылку' )
+				print(e)
 
-		# !!!	
-			# сделать скрол страницы 
-			# так что бы выбранный элемент стал видимым
-		# !!!
+
 
 
 		# Собрать все ссілки на страницы фильмов
@@ -42,18 +45,24 @@ def main_bot_PF_1(URL,browser):
 		time.sleep(random.uniform(1,30))		# случайное число с плавающей точкой
 
 		count_try_clicks_films = 0
+		rnd_film = random.choice(list_elements)
 		while True:
 			try:
 				element_Wait = WebDriverWait(browser, 30).until(
 					EC.element_to_be_clickable((By.XPATH, '//html/body/div[1]/div/div/div/article/div/div/div/a/h2'))
 				)
-				time.sleep(random.uniform(1,30))		# случайное число с плавающей точкой
-				random.choice(list_elements).click()	# выбор случайной ссылки из списка и переход по ней на страницу фильма
+
+				time.sleep(random.uniform(1,10))		# случайное число с плавающей точкой
+				scroll_element(rnd_film,browser)
+				time.sleep(random.uniform(1,10))		# случайное число с плавающей точкой
+
+				rnd_film.click()	# выбор случайной ссылки из списка и переход по ней на страницу фильма
 				break
 			except Exception as e:
 				# raise e
 				count_try_clicks_films += 1
-				print(count_try_clicks_links, '. попытка кликнуть случайный фильм' )
+				print(count_try_clicks_films, '. попытка кликнуть случайный фильм' )
+				print(e)
 
 		# запускаю плеер
 		time.sleep(random.uniform(1,30))		# случайное число с плавающей точкой
@@ -71,5 +80,31 @@ def main_bot_PF_1(URL,browser):
 
 	finally:
 		pass
-		browser.close()
-		browser.quit()
+
+def scroll_element(target_element, browser, scroll_element='body'):
+	'''
+		плавная прокрутка окна до указанных заданного элемента
+	'''
+	rect_elenent = target_element.rect
+
+	x = 0
+	y = 0
+	x_end = rect_elenent["x"]
+	y_end = rect_elenent["y"]
+
+	js_scroll = 'window.scrollTo(' + repr(x_end) + ',' + repr(y_end) + ', document.' + scroll_element + '.scrollHeight);'
+	browser.execute_script(js_scroll)
+
+	# добавить плавность прокрутки
+	# этот вариант плавной прокрутки не работает
+	# while (x < x_end):
+	# 	js_scroll = 'window.scrollTo(' + repr(x) + ',' + repr(y) + ', document.' + scroll_element + '.scrollHeight);'
+	# 	browser.execute_script(js_scroll)
+	# 	time.sleep(random.uniform(5,10))		# случайное число с плавающей точкой
+	# 	x += 1
+
+	# 	print("js_scroll:  ", js_scroll)
+	# 	print('x = ', x)
+
+
+
--- for_my_sites/for_prostofilm.ml/bot_PF_1/main_PF_1.py
+++ for_my_sites/for_prostofilm.ml/bot_PF_1/main_PF_1.py
@@ -1,39 +1,60 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import json
+import sys
 
-import bot_PF_1 
-# from bot_PF_1 import main_bot_PF_1
+# подключаю свои модули:
+sys.path.append(os.path.join( os.path.dirname( __file__ ), '../../../moduls' ))
+import functions_BfS 
+import ParsProxy.pars_proxy as ParsProxy
 
 
 
 def main():
+
+	'''
+		Получить установленное количество запусков на текущие сутки 
+		Проверить количество выполненных запусков за текущие сутки
+		если запуски ещё нужны
+			Проверить наличие актуального proxyList_file
+			если нет 
+				Запустить ParsProxy.get_ProxyList()
+			если запуск ParsProxy.get_ProxyList() был успешен
+				Запустить  bot_PF_1() через proxy (ParsProxy.run_ThroughProxy(bot_PF_1,proxyList))
+			если запуск bot_PF_1() через proxy был успешен
+				Увеличить счётчик успешных запусков на 1
+				собрать статистику
+				сохранить статистику
+				если нужно 
+					Запустить  bot_PF_1() через proxy (ParsProxy.run_ThroughProxy(bot_PF_1,proxyList))
+
+	'''
+
 	
+	proxyList_file = 'proxylist 03-03-2021 16.12.57 .json'		# актуальный прокси лист
 	URL = 'http://prostofilm.ml/'
+	
+	amounth_start_of_bot = 3
+	count_start_of_bot = 0
+	count_proxyIP = 0
+
 
 	# URL = 'http://prostofilm.ml/films/1034049/'	# for tests
 	# URL = 'https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html'		# страница тестирования невидимости автоматического управления браузером
 
-	pathDriver = os.path.dirname(os.path.abspath(__file__)) + "\\geckodriver.exe"
-	options = Options()
-	options.set_preference("dom.webdriver.enabled", False)
-	browser = webdriver.Firefox(executable_path=pathDriver,options=options)
+	path_proxyList_dir = os.path.join( os.path.dirname( __file__ ), '../../../../ParsProxy/Proxylist/' + proxyList_file )
+	with open(path_proxyList_dir) as file_handle:	# получаю прокси из файла в список
+		list_Proxy = json.load(file_handle)
 
 
-	bot_PF_1.main_bot_PF_1(URL,browser)
+	
+
+	while (amounth_start_of_bot > count_start_of_bot):
+		
 
 
-
-
-
-
-
-
-
-
-
-
-
+		run_ThroughProxy(function,proxylist)
 
 
 
